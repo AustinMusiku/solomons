f3cca9554fab710d35e55528483bb216530717e1
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2021-06-30 18:47:42 +0300

    mobile nav

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,8 @@
 
 let menu = document.querySelector('.ham-menu');
-let ham1 = document.querySelector('.ham-layer-left');
-let ham2 = document.querySelector('.ham-layer-right');
+let ham1 = document.querySelector('.ham-layer-top');
+let ham2 = document.querySelector('.ham-layer-mid');
+let ham3 = document.querySelector('.ham-layer-bottom');
 let navLinks = document.querySelector('.nav-links');
 let navLinkButtons = document.querySelectorAll('.nav-link');
 let navCartTotal = document.querySelector('.cart-total');
@@ -11,6 +12,7 @@ menu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     ham1.classList.toggle('active');
     ham2.classList.toggle('active');
+    ham3.classList.toggle('active');
 })
 
 navLinkButtons.forEach(btn => {
@@ -18,6 +20,7 @@ navLinkButtons.forEach(btn => {
         navLinks.classList.toggle('active');
         ham1.classList.toggle('active');
         ham2.classList.toggle('active');
+        ham3.classList.toggle('active');
     })
 })
 
